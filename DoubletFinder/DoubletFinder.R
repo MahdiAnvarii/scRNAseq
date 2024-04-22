8ee970e05a923df1dba76f701c03d95df6202577
Mahdi <125554535+MahdiAnvarii@users.noreply.github.com>
2024-04-22 20:18:19 +0330

    Update DoubletFinder.R

--- DoubletFinder/DoubletFinder.R
+++ DoubletFinder/DoubletFinder.R
@@ -47,3 +47,16 @@ Seurat_obj <- FindNeighbors(object = Seurat_obj, dims = 1:20)
 Seurat_obj <- FindClusters(object = Seurat_obj)
 # Non-Linear
 Seurat_obj <- RunUMAP(Seurat_obj , dims = 1:20)
+
+# pK identification
+sweep_res_list <- paramSweep(Seurat_obj, PCs = 1:20, sct = FALSE)
+view(sweap_res_list)
+sweep_states <- summarizeSweep(sweep_res_list, GT = FALSE)
+view(sweep_states)
+bcmvn <- find.pK(sweep_states)
+view(bcmvn)
+pkval <- ggplot(bcmvn, aes(pK, BCmetric, group = 1))+ geom_point() + geom_line()
+ggsave("pK_values_plot.pdf", plot = pkval, device = "pdf", width = 16 , height = 10)
+
+
+
