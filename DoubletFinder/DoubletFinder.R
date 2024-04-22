fe627b8bffefb7b3f1f52274efe96ce060ffc292
Mahdi <125554535+MahdiAnvarii@users.noreply.github.com>
2024-04-22 11:03:43 +0330

    Update DoubletFinder.R

--- DoubletFinder/DoubletFinder.R
+++ DoubletFinder/DoubletFinder.R
@@ -5,3 +5,17 @@ library(DoubletFinder)
 library(ggplot2)
 library(tidyverse)
 library(Seurat)
+
+cts <- ReadMtx(mtx = '../Data/10k_PBMC_3p_nextgem_Chromium_Controller_raw_feature_bc_matrix/matrix.mtx.gz',
+        features = '../Data/10k_PBMC_3p_nextgem_Chromium_Controller_raw_feature_bc_matrix/features.tsv.gz',
+        cells = '../Data/10k_PBMC_3p_nextgem_Chromium_Controller_raw_feature_bc_matrix/barcodes.tsv.gz')
+str(cts)
+nrow(cts)
+ncol(cts)
+cts[1:20,1:20]
+
+Seurat_obj <- CreateSeuratObject(counts = cts , project = "PBMC")
+str(Seurat_obj)
+nrow(Seurat_obj)
+ncol(Seurat_obj)
+view(Seurat_obj@meta.data)
