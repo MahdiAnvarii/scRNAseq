74416401781f018bc371e4e3148d3aed2f10ee00
Mahdi <125554535+MahdiAnvarii@users.noreply.github.com>
2024-05-04 19:50:47 +0330

    Update FindMarkers.R

--- FindMarkers/FindMarkers.R
+++ FindMarkers/FindMarkers.R
@@ -9,5 +9,33 @@ install.packages("https://seurat.nygenome.org/src/contrib/ifnb.SeuratData_3.0.0.
 library(ifnb.SeuratData)
 InstallData("ifnb")
 data("ifnb")
-str(ifnb)
-View(ifnb@meta.data)
+ifnb.updated = UpdateSeuratObject(object = ifnb)
+str(ifnb.updated)
+View(ifnb.updated@meta.data)
+dim(ifnb.updated)
+
+# QC
+ifnb.updated[["MT_percent"]] <- PercentageFeatureSet(ifnb.updated , patter = "^MT-")
+view(ifnb.updated@meta.data)
+
+# Filtering
+ifnb.updated <- subset(ifnb.updated, subset = nFeature_RNA > 200 & nCount_RNA > 800 & MT_percent < 5)
+str(ifnb.updated)
+dim(ifnb.updated)
+view(ifnb.updated@meta.data)
+
+### Preprocess workflow
+# Normalization
+ifnb.updated <- NormalizeData(object = ifnb.updated)
+# Collect highly variable features
+ifnb.updated <- FindVariableFeatures(object = ifnb.updated)
+# Scaling
+ifnb.updated <- ScaleData(object = ifnb.updated)
+# PCA
+ifnb.updated <- RunPCA(object = ifnb.updated)
+elbowplot <- ElbowPlot(ifnb.updated)
+ggsave("elbow_plot.pdf", plot = elbowplot, device = "pdf")
+# Non-Linear
+ifnb.updated <- RunUMAP(ifnb.updated , dims = 1:20 , reduction = 'pca')
+dimplot <- DimPlot(ifnb.updated, reduction = "umap", group.by = 'stim')
+ggsave("dim_plot.pdf", plot = dimplot, device = "pdf")
