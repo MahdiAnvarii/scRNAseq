52a6fb8f03ff0533287d6a1cb105ff6e8fea3608
Mahdi <125554535+MahdiAnvarii@users.noreply.github.com>
2024-05-04 20:24:44 +0330

    Update FindMarkers.R

--- FindMarkers/FindMarkers.R
+++ FindMarkers/FindMarkers.R
@@ -39,5 +39,21 @@ ggsave("elbow_plot.pdf", plot = elbowplot, device = "pdf")
 # Non-Linear
 ifnb.updated <- RunUMAP(ifnb.updated , dims = 1:20 , reduction = 'pca')
 dimplot <- DimPlot(ifnb.updated, reduction = "umap", group.by = 'stim')
-ggsave("before_dim_plot.pdf", plot = dimplot, device = "pdf")
+ggsave("before_dim_plot.pdf", plot = dimplot, device = "pdf", width = 16 , height = 10)
 
+# Harmony
+ifnb.harmony <- ifnb.updated %>%
+  RunHarmony(group.by.vars = 'stim' , plot_convergence = FALSE)
+ifnb.harmony@reductions
+
+ifnb.harmony.embed <- Embeddings(ifnb.harmony , "harmony")
+ifnb.harmony.embed[1:20,1:20]
+
+ifnb.harmony <- ifnb.harmony %>%
+  RunUMAP(reduction = 'harmony' , dims = 1:20) %>%
+  FindNeighbors(reduction = 'harmony' , dims = 1:20) %>%
+  FindClusters(resolution = 0.5)
+
+dimplot2 <- DimPlot(ifnb.harmony, reduction = "umap", group.by = 'stim')
+ggsave("after_dim_plot.pdf", plot = dimplot2, device = "pdf", width = 16 , height = 10)
+dimplot|dimplot2
