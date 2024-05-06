a58cc3f9cbe625e951ff7f72067d20983d658225
Mahdi <125554535+MahdiAnvarii@users.noreply.github.com>
2024-05-06 11:40:50 +0330

    Update PseudoBulk.R

--- PseudoBulk/PseudoBulk.R
+++ PseudoBulk/PseudoBulk.R
@@ -8,3 +8,11 @@ install.packages("AnnotationHub")
 library(AnnotationHub)
 library(ExperimentHub)
 
+experimenthub = ExperimentHub(localHub=FALSE)
+query(experimenthub,"Kang")
+install.packages("SingleCellExperiment")
+library(SingleCellExperiment)
+install.packages("muscData")
+library(muscData)
+sce_obj <- experimenthub[['EH2259']]
+sce_obj
