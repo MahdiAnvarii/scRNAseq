88127253ae40a9ba7c3aec1a488bef5633947b34
Mahdi <125554535+MahdiAnvarii@users.noreply.github.com>
2024-05-06 13:55:36 +0330

    Update PseudoBulk.R

--- PseudoBulk/PseudoBulk.R
+++ PseudoBulk/PseudoBulk.R
@@ -32,3 +32,23 @@ Seurat_obj <- subset(Seurat_obj, subset = nFeature_originalexp > 200 & nCount_or
 str(Seurat_obj)
 dim(Seurat_obj)
 view(Seurat_obj@meta.data)
+
+### Preprocess workflow
+# Normalization
+Seurat_obj <- NormalizeData(object = Seurat_obj)
+# Collect highly variable features
+Seurat_obj <- FindVariableFeatures(object = Seurat_obj)
+# Scaling
+Seurat_obj <- ScaleData(object = Seurat_obj)
+# PCA
+Seurat_obj <- RunPCA(object = Seurat_obj)
+elbowplot <- ElbowPlot(Seurat_obj)
+ggsave("elbow_plot.pdf", plot = elbowplot, device = "pdf")
+# Non-Linear
+Seurat_obj <- RunUMAP(Seurat_obj , dims = 1:20)
+dimplot1 <- DimPlot(Seurat_obj, reduction = "umap", group.by = 'cell', label = TRUE)
+ggsave("cell_types_dim_plot.pdf", plot = dimplot1, device = "pdf", width = 16 , height = 10)
+dimplot2 <- DimPlot(Seurat_obj, reduction = "umap", group.by = 'stim')
+ggsave("conditions_dim_plot.pdf", plot = dimplot2, device = "pdf", width = 16 , height = 10)
+cc <- dimplot1|dimplot2
+ggsave("conditions_clusters_dim_plot.pdf", plot = cc, device = "pdf", width = 16 , height = 10)
