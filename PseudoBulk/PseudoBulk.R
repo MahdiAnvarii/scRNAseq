0f7e04c3827798d9553a9ac8c486e8b40a7aa525
Mahdi <125554535+MahdiAnvarii@users.noreply.github.com>
2024-05-06 11:52:37 +0330

    Update PseudoBulk.R

--- PseudoBulk/PseudoBulk.R
+++ PseudoBulk/PseudoBulk.R
@@ -16,3 +16,19 @@ install.packages("muscData")
 library(muscData)
 sce_obj <- experimenthub[['EH2259']]
 sce_obj
+
+Seurat_obj <- as.Seurat(sce_obj , data = NULL)
+str(Seurat_obj)
+dim(Seurat_obj)
+view(Seurat_obj@meta.data)
+
+# QC
+Seurat_obj[["MT_percent"]] <- PercentageFeatureSet(Seurat_obj , patter = "^MT-")
+view(Seurat_obj@meta.data)
+
+# Filtering
+Seurat_obj <- subset(Seurat_obj, subset = nFeature_originalexp > 200 & nCount_originalexp > 800 
+                     & MT_percent < 5 & multiplets == 'singlet')
+str(Seurat_obj)
+dim(Seurat_obj)
+view(Seurat_obj@meta.data)
