d3993eb692dbdb2437e737840b5608ffc1785acf
Mahdi <125554535+MahdiAnvarii@users.noreply.github.com>
2024-05-07 15:17:05 +0330

    Update Trajectory.R

--- Trajectory/Trajectory.R
+++ Trajectory/Trajectory.R
@@ -7,3 +7,10 @@ library(SeuratWrappers)
 library(Seurat)
 library(ggplot2)
 library(tidyverse)
+
+markers <- read.delim('../Data/ABC_Marker.txt' , header = T) # gene metadata
+metadata <- read.delim('../Data/ABC_Meta.txt' , header = T) # cells metadata
+expressions <- read.delim('../Data/ABC_umi_matrix_7551_cells.csv' , header = T , sep = ',') # expression matrix
+head(markers)
+head(metadata)
+expressions[1:10,1:10]
