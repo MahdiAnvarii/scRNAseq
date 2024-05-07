f883475a8739dba3af80803e94a3b41a6253ba74
Mahdi <125554535+MahdiAnvarii@users.noreply.github.com>
2024-05-07 16:40:16 +0330

    Update Trajectory.R

--- Trajectory/Trajectory.R
+++ Trajectory/Trajectory.R
@@ -23,3 +23,24 @@ Seurat_obj@meta.data <- merge(Seurat_obj@meta.data, metadata, by.x = 'row.names'
 view(Seurat_obj@meta.data)
 Seurat_obj@meta.data <- Seurat_obj@meta.data %>%
   column_to_rownames(var = 'Row.names')
+str(Seurat_obj)
+dim(Seurat_obj)
+
+# QC
+Seurat_obj@meta.data$MT_percent <- PercentageFeatureSet(Seurat_obj , patter = "^MT-")
+view(Seurat_obj@meta.data)
+
+# Filtering
+Seurat_obj <- subset(Seurat_obj, subset = nFeature_RNA > 200 & nCount_RNA > 800 & MT_percent < 5)
+str(Seurat_obj)
+dim(Seurat_obj)
+view(Seurat_obj@meta.data)
+
+# Choosing B cells
+unique(Seurat_obj@meta.data$population)
+Idents(Seurat_obj) <- Seurat_obj$population
+B_Seurat_obj <- subset(Seurat_obj, idents = 'b')
+str(B_Seurat_obj)
+dim(B_Seurat_obj)
+view(B_Seurat_obj@meta.data)
+unique(B_Seurat_obj@meta.data$redefined_cluster)
