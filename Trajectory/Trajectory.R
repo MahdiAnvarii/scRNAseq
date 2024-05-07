f8a80d4f1d3009bc1f610516ad342bf453541a7b
Mahdi <125554535+MahdiAnvarii@users.noreply.github.com>
2024-05-07 14:46:31 +0330

    Create Trajectory.R

---
+++ Trajectory/Trajectory.R
@@ -0,0 +1,9 @@
+library(devtools)
+devtools::install_github('satijalab/seurat-wrappers')
+devtools::install_github('cole-trapnell-lab/monocle3')
+
+library(monocle3)
+library(SeuratWrappers)
+library(Seurat)
+library(ggplot2)
+library(tidyverse)
